73e26c44a35baaf3e7cde12960e8a6089ac170e8
nguyennamdev <namcoithang8@gmail.com>
2018-05-08 17:54:10 +0700

    add actions on history cell

--- WheelShip/WheelShip/OrdersHistoryCollectionCell.swift
+++ WheelShip/WheelShip/OrdersHistoryCollectionCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class OrdersHistoryCollectionCell : BaseCell {
-    
+    var isShowingActions:Bool = false
     var oder:Order?{
         didSet{
             showingDetailContainer()
@@ -27,6 +27,9 @@ class OrdersHistoryCollectionCell : BaseCell {
             }
         }
     }
+    
+    var rightConstraintContainerView:NSLayoutConstraint?
+    var leftConstaintContainnerView:NSLayoutConstraint?
     
     // closure attitudeString
     let attitudeString = { (title:(String,UIColor),content:(String, UIColor)) -> NSMutableAttributedString in
@@ -46,6 +49,10 @@ class OrdersHistoryCollectionCell : BaseCell {
         setupPhoneReceiverLabel()
         setupDownArrowView()
         setupDetailContainer()
+        setupActionView()
+        self.isUserInteractionEnabled = true
+        self.addGestureRecognizer(leftSwipeGesture)
+        self.addGestureRecognizer(righSwipeGesture)
     }
     
     // MARK: Private functions
@@ -96,11 +103,44 @@ class OrdersHistoryCollectionCell : BaseCell {
         overheadsLabel.attributedText = attitudeString((Define.OVERHEADS, #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)), (" \(overheads.formatedNumberWithUnderDots())", UIColor.white))
     }
     
+    // MARK: Actions
     
+    @objc private func showActions(){
+        if !isShowingActions {
+            // margin 8 value
+            self.rightConstraintContainerView?.constant = -58
+            self.leftConstaintContainnerView?.constant = -58
+            self.actionView.isHidden = false
+        }
+        UIView.animate(withDuration: 0.5) {
+            self.layoutIfNeeded()
+        }
+        isShowingActions = !isShowingActions
+    }
+    
+    @objc private func hideActions(){
+        if isShowingActions {
+            self.rightConstraintContainerView?.constant = -12
+            self.leftConstaintContainnerView?.constant = 12
+            self.actionView.isHidden = true
+        }
+        UIView.animate(withDuration: 0.5) {
+            self.layoutIfNeeded()
+        }
+        isShowingActions = !isShowingActions
+    }
+
     // MARK:setup view
     private func setContainView(){
         addSubview(containerView)
-        containerView.anchorWithConstants(top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 12, bottomConstant: 0, rightConstant: 12)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        // left, right constaint
+        leftConstaintContainnerView = NSLayoutConstraint(item: containerView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 12)
+        rightConstraintContainerView = NSLayoutConstraint(item: containerView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: -12)
+        self.addConstraint(leftConstaintContainnerView!)
+        self.addConstraint(rightConstraintContainerView!)
         containerView.addSubview(gradient)
         gradient.anchorWithConstants(top: self.containerView.topAnchor, left: self.containerView.leftAnchor, bottom: self.containerView.bottomAnchor, right: self.containerView.rightAnchor)
     }
@@ -163,11 +203,24 @@ class OrdersHistoryCollectionCell : BaseCell {
         detailContainer.addArrangedSubview(priceOfWeightLabel)
     }
     
+    private func setupActionView(){
+        self.addSubview(actionView)
+        actionView.anchorWithWidthHeightConstant(top: self.topAnchor, left: containerView.rightAnchor, bottom: self.bottomAnchor, right: nil, topConstant: 0, leftConstant: 4, bottomConstant: 0, rightConstant: 0, widthConstant: 50)
+        // set up subviews
+        actionView.addSubview(editActionButton)
+        actionView.addSubview(deleteActionButton)
+        editActionButton.anchorWithConstants(top: actionView.topAnchor, left: actionView.leftAnchor, bottom: nil, right: actionView.rightAnchor)
+        editActionButton.heightAnchor.constraint(equalTo: actionView.heightAnchor, multiplier: 0.5).isActive = true
+        
+        deleteActionButton.anchorWithConstants(top: editActionButton.bottomAnchor, left: actionView.leftAnchor, bottom: actionView.bottomAnchor, right: actionView.rightAnchor)
+    }
+    
     // MARK: Views
     let containerView:UIView = {
         let view = UIView()
         view.clipsToBounds = true
         view.layer.cornerRadius = 5
+        view.isUserInteractionEnabled = true
         return view
     }()
     
@@ -258,4 +311,43 @@ class OrdersHistoryCollectionCell : BaseCell {
         let label = UILabel()
         return label
     }()
+    
+    lazy var leftSwipeGesture:UISwipeGestureRecognizer = {
+        let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(showActions))
+        swipeGesture.direction = .left
+        return swipeGesture
+    }()
+    
+    lazy var righSwipeGesture:UISwipeGestureRecognizer  = {
+        let swipteGesture = UISwipeGestureRecognizer(target: self, action: #selector(hideActions))
+        swipteGesture.direction = .right
+        return swipteGesture
+    }()
+    
+    let actionView:UIView = {
+        let view = UIView()
+        //34,193,195
+        view.backgroundColor = UIColor.rgb(r: 34, g: 193, b: 195)
+        view.layer.cornerRadius = 5
+        view.clipsToBounds = true
+        view.isHidden = true
+        return view
+    }()
+    
+    lazy var editActionButton:UIButton = {
+        let button = UIButton()
+        //86,204,242
+        button.backgroundColor = UIColor.rgb(r: 86, g: 204, b: 242)
+        button.setImage(#imageLiteral(resourceName: "edit"), for: .normal)
+        return button
+    }()
+    
+    lazy var deleteActionButton:UIButton = {
+        let button = UIButton()
+        //240,80,83
+        button.backgroundColor = UIColor.rgb(r: 240, g: 80, b: 83)
+        button.setImage(#imageLiteral(resourceName: "delete"), for: .normal)
+        return button
+    }()
+    
 }
